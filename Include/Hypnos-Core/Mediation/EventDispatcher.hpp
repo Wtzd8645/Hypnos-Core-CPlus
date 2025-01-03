b17e96afad426b3be4a00ac9c09b8d15a008ae59
Neo <wtzd8645@gmail.com>
2025-01-04 00:05:37 +0800

    fix(mediation): Fix incorrect variable type

--- Include/Hypnos-Core/Mediation/EventDispatcher.hpp
+++ Include/Hypnos-Core/Mediation/EventDispatcher.hpp
@@ -2,51 +2,16 @@
 
 #include "Hypnos-Core/Container/UnorderedMap.hpp"
 
+// TODO: Decouple from Container::UnorderedMap.
 namespace Blanketmen {
 namespace Hypnos {
-
-class EventHandlerBase
-{
-public:
-    virtual ~EventHandlerBase() = default;
-    virtual void Handle() = 0;
-};
 
 template<typename... T>
 class EventHandler
 {
 public:
     virtual ~EventHandler() = default;
-    virtual void Handle(T&&... args) = 0;
-};
-
-template<typename TKey>
-class EventDispatcherBase
-{
-public:
-    virtual ~EventDispatcherBase() = default;
-
-    void Register(TKey id, EventHandlerBase* handler)
-    {
-        handlerMap[id] = handler;
-    }
-
-    void Unregister(TKey id)
-    {
-        handlerMap.erase(id);
-    }
-
-    void Dispatch(TKey id)
-    {
-        auto it = handlerMap.find(id);
-        if (it != handlerMap.end())
-        {
-            it->second.Handle(id);
-        }
-    }
-
-private:
-    Container::UnorderedMap<TKey, EventHandlerBase&> handlerMap;
+    virtual void Handle(T... args) = 0;
 };
 
 template<typename TKey, typename... TArgs>
@@ -65,17 +30,18 @@ public:
         handlerMap.erase(id);
     }
 
-    void Dispatch(TKey id, TArgs&&... args)
+protected:
+    void Dispatch(TKey id, TArgs... args) const
     {
         auto it = handlerMap.find(id);
         if (it != handlerMap.end())
         {
-            it->second.Handle(std::forward<TArgs>(args)...);
+            it->second->Handle(std::forward<TArgs>(args)...);
         }
     }
 
 private:
-    Container::UnorderedMap<TKey, EventHandler<TArgs...>&> handlerMap;
+    Container::UnorderedMap<TKey, EventHandler<TArgs...>*> handlerMap;
 };
 
 } // namespace Hypnos
