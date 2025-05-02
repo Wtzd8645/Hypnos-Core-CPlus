9fb7b2d272b063da0480c68f1ecd545517726eba
Neo <wtzd8645@gmail.com>
2025-05-03 00:10:46 +0800

    fix: Use memcpy instead of reinterpret to avoid alignment issues

--- Runtime/Core/IO/BinaryReader.hpp
+++ Runtime/Core/IO/BinaryReader.hpp
@@ -1,78 +1,71 @@
 #pragma once
 
 #include "Platform.hpp"
+#include <cstring>
 
 namespace Blanketmen {
 namespace Hypnos {
 namespace IO {
 
-inline void ReadInt8(int8& result, uint8* buffer, size_t offset)
+struct BinaryReader
 {
-    result = *reinterpret_cast<int8*>(buffer + offset);
-    offset += sizeof(int8);
-}
+    inline static void ReadInt8(int8& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(int8));
+    }
 
-inline void ReadUInt8(uint8& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<uint8*>(buffer + offset);
-    offset += sizeof(uint8);
-}
+    inline static void ReadUInt8(uint8& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(uint8));
+    }
 
-inline void ReadInt16(int16& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<int16*>(buffer + offset);
-    offset += sizeof(int16);
-}
+    inline static void ReadInt16(int16& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(int16));
+    }
 
-inline void ReadUInt16(uint16& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<uint16*>(buffer + offset);
-    offset += sizeof(uint16);
-}
+    inline static void ReadUInt16(uint16& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(uint16));
+    }
 
-inline void ReadInt32(int32& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<int32*>(buffer + offset);
-    offset += sizeof(int32);
-}
+    inline static void ReadInt32(int32& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(int32));
+    }
 
-inline void ReadUInt32(uint32& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<uint32*>(buffer + offset);
-    offset += sizeof(uint32);
-}
+    inline static void ReadUInt32(uint32& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(uint32));
+    }
 
-inline void ReadInt64(int64& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<int64*>(buffer + offset);
-    offset += sizeof(int64);
-}
+    inline static void ReadInt64(int64& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(int64));
+    }
 
-inline void ReadUInt64(uint64& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<uint64*>(buffer + offset);
-    offset += sizeof(uint64);
-}
+    inline static void ReadUInt64(uint64& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(uint64));
+    }
 
-inline void ReadFloat32(float32& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<float32*>(buffer + offset);
-    offset += sizeof(float32);
-}
+    inline static void ReadFloat32(float32& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(float32));
+    }
 
-inline void ReadFloat64(float64& result, uint8* buffer, size_t offset)
-{
-    result = *reinterpret_cast<float64*>(buffer + offset);
-    offset += sizeof(float64);
-}
+    inline static void ReadFloat64(float64& result, uint8* buffer)
+    {
+        std::memcpy(&result, buffer, sizeof(float64));
+    }
 
-inline void ReadString(string& result, uint8* buffer, size_t offset)
-{
-    int len = *reinterpret_cast<uint16*>(buffer + offset);
-    offset += sizeof(uint16);
-    result.assign(reinterpret_cast<char*>(buffer + offset), len);
-    offset += len;
-}
+    inline static void ReadString(string& result, uint8* buffer)
+    {
+        size_t len;
+        std::memcpy(&len, buffer, sizeof(size_t));
+        result.assign(reinterpret_cast<char*>(buffer + sizeof(size_t)), len);
+    }
+};
 
 } // namespace IO
 } // namespace Hypnos
--- Runtime/Core/IO/BinaryWriter.hpp
+++ Runtime/Core/IO/BinaryWriter.hpp
@@ -7,75 +7,65 @@ namespace Blanketmen {
 namespace Hypnos {
 namespace IO {
 
-inline void WriteInt8(int8& value, uint8* buffer, size_t offset)
+struct BinaryWriter
 {
-    *reinterpret_cast<int8*>(buffer + offset) = value;
-    offset += sizeof(int8);
-}
+    inline static void WriteInt8(int8& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(int8));
+    }
 
-inline void WriteUInt8(uint8& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<uint8*>(buffer + offset) = value;
-    offset += sizeof(uint8);
-}
+    inline static void WriteUInt8(uint8& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(uint8));
+    }
 
-inline void WriteInt16(int16& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<int16*>(buffer + offset) = value;
-    offset += sizeof(int16);
-}
+    inline static void WriteInt16(int16& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(int16));
+    }
 
-inline void WriteUInt16(uint16& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<uint16*>(buffer + offset) = value;
-    offset += sizeof(uint16);
-}
+    inline static void WriteUInt16(uint16& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(uint16));
+    }
 
-inline void WriteInt32(int32& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<int32*>(buffer + offset) = value;
-    offset += sizeof(int32);
-}
+    inline static void WriteInt32(int32& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(int32));
+    }
 
-inline void WriteUInt32(uint32& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<uint32*>(buffer + offset) = value;
-    offset += sizeof(uint32);
-}
+    inline static void WriteUInt32(uint32& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(uint32));
+    }
 
-inline void WriteInt64(int64& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<int64*>(buffer + offset) = value;
-    offset += sizeof(int64);
-}
+    inline static void WriteInt64(int64& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(int64));
+    }
 
-inline void WriteUInt64(uint64& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<uint64*>(buffer + offset) = value;
-    offset += sizeof(uint64);
-}
+    inline static void WriteUInt64(uint64& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(uint64));
+    }
 
-inline void WriteFloat32(float32& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<float32*>(buffer + offset) = value;
-    offset += sizeof(float32);
-}
+    inline static void WriteFloat32(float32& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(float32));
+    }
 
-inline void WriteFloat64(float64& value, uint8* buffer, size_t offset)
-{
-    *reinterpret_cast<float64*>(buffer + offset) = value;
-    offset += sizeof(float64);
-}
+    inline static void WriteFloat64(float64& value, uint8* buffer)
+    {
+        std::memcpy(buffer, &value, sizeof(float64));
+    }
 
-inline void WriteString(string& value, uint8* buffer, size_t offset)
-{
-    uint16 len = value.length();
-    *reinterpret_cast<uint16*>(buffer + offset) = len;
-    offset += sizeof(uint16);
-
-    std::strncpy(reinterpret_cast<char*>(buffer + offset), value.c_str(), len);
-    offset += len;
-}
+    inline static void WriteString(string& value, uint8* buffer)
+    {
+        size_t len = value.length();
+        std::memcpy(buffer, &len, sizeof(size_t));
+        std::memcpy(buffer + sizeof(size_t), value.c_str(), len);
+    }
+};
 
 } // namespace IO
 } // namespace Hypnos
